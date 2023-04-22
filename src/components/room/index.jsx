f140e9c421e584b115812e1a21cd69b66596ab2c
Jackson de Almeida <jacksonalmeida@tokenlab.com.br>
2023-04-22 16:03:15 -0300

    Layout updated

--- src/components/room/index.jsx
+++ src/components/room/index.jsx
@@ -1,45 +1,82 @@
-import React from "react"
+import React, { useState } from "react"
 import './index.css';
 
 import { Button, DatePicker, Input } from 'antd';
 import { Breadcrumb, Layout, Menu, theme, Col, Row, Space } from 'antd';
+import { VideoCameraOutlined, WindowsOutlined, AudioOutlined } from "@ant-design/icons";
 const { Header, Content, Footer } = Layout;
 
 function RoomComponent() {
 
+  const [cameraOn, setCameraOn] = useState(false);
+  const [screenSharedOn, setScreenSharedOn] = useState(false);
+  const [micOn, setMicOn] = useState(false);
 
   return (
     <Layout className="layout-area">
       <Header className="header-area">
-        <div className="logo" />
-        <h1>Fala ai!</h1>
+        <div>
+          <span className="logo">Fala Ai!</span>
+        </div>
+        <div>
+          <Space.Compact style={{ width: '100%' }}>
+            <Input defaultValue="" placeholder="Digite seu nome..." />
+            <Button type="primary">Entrar</Button>
+          </Space.Compact>
+        </div>
       </Header>
-      <Row style={{ height: 'calc(100vh - 144px)' }}>
-        <Col sm={14} md={16} lg={16} xl={18}>
+      <Row style={{ height: 'calc(100vh)' }}>
+        <Col sm={14} md={16} lg={16} xl={18} className="video-grid">
           <Content className="video-area">
             <video src="" className="video"></video>
           </Content>
+          <Footer className="footer-area" style={{ }}>
+            <Button
+              onClick={() => setCameraOn(!cameraOn)}
+              style={{
+                background: cameraOn ? '' : 'red',
+                color: cameraOn ? '' : '#FFF'
+              }}>
+              <VideoCameraOutlined />
+            </Button>
+            <Button
+              onClick={() => setScreenSharedOn(!screenSharedOn)}
+              style={{
+                background: screenSharedOn ? '' : '#060212',
+                color: screenSharedOn ? '' : '#FFF'
+              }}>
+              <WindowsOutlined />
+            </Button>
+            <Button
+              onClick={() => setMicOn(!micOn)}
+              style={{
+                background: micOn ? '' : 'red',
+                color: micOn ? '' : '#FFF'
+              }}>
+              <AudioOutlined />
+            </Button>
+            {/* Fala Ai! ©2023 Created by Jackson & Gabriel */}
+          </Footer>
         </Col>
         <Col sm={10} md={8} lg={8} xl={6}>
           <Content className="chat-area">
             <div className="chat">
               teste
             </div>
-            <div className="new-text-area" style={{}}>
+            <div className="new-text-area">
               <Space.Compact style={{ width: '100%' }}>
                 <Input defaultValue="" placeholder="Digite..." />
-                <Button type="primary">Submit</Button>
+                <Button type="primary">Enviar</Button>
               </Space.Compact>
             </div>
           </Content>
         </Col>
       </Row>
-      <Footer className="footer-area" style={{ }}>
+      {/* <Footer className="footer-area" style={{ }}>
         <Button>Camera On</Button>
         <Button>Compartilhar Tela</Button>
         <Button>Microfone On</Button>
-        {/* Fala Ai! ©2023 Created by Jackson & Gabriel */}
-      </Footer>
+      </Footer> */}
     </Layout>
   )
 }
